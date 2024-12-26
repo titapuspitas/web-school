2304ba7ff36d6b30f62a0a2bbc29b35291323923
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2024-12-26 13:28:15 +0700

    nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  css: ["bootstrap/dist/css/bootstrap.min.css"],
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",
+    "bootstrap-icons/font/bootstrap-icons.min.css"
+    ],
   app: {
     head: {
       script: [{
